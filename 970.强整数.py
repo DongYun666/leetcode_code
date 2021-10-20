d71192ebc2aefb2fc12fddbe50a50e555e5cd323
DongYun666 <1104418378@qq.com>
2021-10-20 10:36:18 +0800

    2021.10.19 刷题

--- 970.强整数.py
+++ 970.强整数.py
@@ -7,17 +7,21 @@ from typing import List
 import math
 class Solution:
     def powerfulIntegers(self, x: int, y: int, bound: int) -> List[int]:
-        if x == 1:
-            count_x = bound
-        elif y==1:
-            count_y = bound
-        else:
-            count_x = int(math.log((bound-1),x))+1
-            count_y = int(math.log((bound-1),y))+1
+        ###报错[2,1,10]-->[9,2,3,5]
+        # count_x,count_y =0,0
+        # if x == 1:
+        #     count_x = bound
+        # elif y==1:
+        #     count_y = bound
+        # else:
+        #     count_x = int(math.log((bound-1),x))+1
+        #     count_y = int(math.log((bound-1),y))+1
         resoult = []
-        for i in range(count_x):
-            for j in range(count_y):
+        for i in range(18):
+            for j in range(18):
                 if x**i+y**j <= bound:
                     resoult.append(x**i+y**j)
         return sorted(set(resoult))
-print(Solution().powerfulIntegers(2,1,10))
+
+
+print(Solution().powerfulIntegers(2,2,40000))
